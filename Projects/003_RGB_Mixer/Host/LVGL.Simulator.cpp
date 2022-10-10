4c6f641dfb7477391b86c25ab7d7905b91ca0425
Kiran Nayak <niekiran@gmail.com>
2022-10-10 15:17:34 +0530

    Update files

---
+++ Projects/003_RGB_Mixer/Host/LVGL.Simulator.cpp
@@ -0,0 +1,62 @@
+﻿/*
+ * PROJECT:   LVGL PC Simulator using Visual Studio
+ * FILE:      LVGL.Simulator.cpp
+ * PURPOSE:   Implementation for LVGL ported to Windows Desktop
+ *
+ * LICENSE:   The MIT License
+ *
+ * DEVELOPER: Mouri_Naruto (Mouri_Naruto AT Outlook.com)
+ */
+
+#include <Windows.h>
+
+#include "resource.h"
+#include "rgb_mixer.h"
+
+#if _MSC_VER >= 1200
+ // Disable compilation warnings.
+#pragma warning(push)
+// nonstandard extension used : bit field types other than int
+#pragma warning(disable:4214)
+// 'conversion' conversion from 'type1' to 'type2', possible loss of data
+#pragma warning(disable:4244)
+#endif
+
+#include "lvgl/lvgl.h"
+#include "lvgl/examples/lv_examples.h"
+#include "lvgl/demos/lv_demos.h"
+#include "lv_drivers/win32drv/win32drv.h"
+
+#if _MSC_VER >= 1200
+// Restore compilation warnings.
+#pragma warning(pop)
+#endif
+
+#include <stdio.h>
+
+int main()
+{
+    lv_init();
+
+    if (!lv_win32_init(
+        GetModuleHandleW(NULL),
+        SW_SHOW,
+        320,
+        240,
+        LoadIconW(GetModuleHandleW(NULL), MAKEINTRESOURCE(IDI_LVGL))))
+    {
+        return -1;
+    }
+
+    lv_win32_add_all_input_devices_to_group(NULL);
+
+    rgb_mixer_create_ui();
+
+    while (!lv_win32_quit_signal)
+    {
+        lv_task_handler();
+        Sleep(1);
+    }
+
+    return 0;
+}
